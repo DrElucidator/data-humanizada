4b8ef1ab2d627453f6fb7fc96d8a686abbd34f06
DrElucidator <dr.elucidator@gmail.com>
2026-08-20 17:54:34 -0300

    Implementado teste de data atual

--- src/DataHumanizada/HumanizadorDeData.cs
+++ src/DataHumanizada/HumanizadorDeData.cs
@@ -2,4 +2,8 @@ namespace DataHumanizada;
 
 public static class HumanizadorDeData
 {
+    public static string Humanizar(DateTime dataInformada, DateTime dataAtual)
+    {
+        return "Agora mesmo";
+    }
 }
--- tests/DataHumanizada.Testes.Unidade/HumanizadorDeDataTests.cs
+++ tests/DataHumanizada.Testes.Unidade/HumanizadorDeDataTests.cs
@@ -3,4 +3,16 @@ namespace DataHumanizada.Testes.Unidade;
 [TestClass]
 public sealed class HumanizadorDeDataTests
 {
+    [TestMethod]
+    public void Humanizar_DataIgualADataAtual_DeveRetornar_AgoraMesmo()
+    {
+        // Arranjo
+        DateTime dataAtual = new(2026, 6, 18, 0, 0, 0);
+
+        // Ação
+        string resultado = HumanizadorDeData.Humanizar(dataAtual, dataAtual);
+
+        // Asserção
+        Assert.AreEqual("Agora mesmo", resultado);
+    }
 }
